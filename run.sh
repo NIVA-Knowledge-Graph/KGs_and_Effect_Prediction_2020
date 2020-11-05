8e9c437c9f4928dd7b57e18c218aa9d0c0d5491f
Erik Bryhn Myklebust <erik.bryhn.myklebust@gmail.com>
2020-11-05 13:00:29 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ python3 models/pretrained_embedding_models.py
 
 for SAMPLING in both none species chemical
     do
-         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 0 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "onehot" 1 --SIMPLE
-         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 0 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "onehot" 1
+         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 20 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "onehot" 1 --SIMPLE
+         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 20 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "onehot" 1
  
-         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 0 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "pretrained" 1 --SIMPLE
-         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 0 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "pretrained" 1 
+         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 20 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "pretrained" 1 --SIMPLE
+         python3 run_all_models.py --NUM_RUNS 10 --MAX_EPOCHS 1000 --PATIENCE 10 --MAX_TRIALS 20 --SEARCH_MAX_EPOCHS 1000 $SAMPLING "pretrained" 1 
 # 
         for nm in 1 2 3 40 81
             do 
